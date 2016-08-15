887b3cf117d4d987b2bd59e3bb59fe0f5a2bb974
halfalpine <rocksandwoks@gmail.com>
2016-08-15 09:48:38 -0400

    Initial commit.

---
+++ checkLetters.js
@@ -0,0 +1,14 @@
+// Create a recursive function to see if an array contains nothing other than numbers.
+
+var a = [1, 2, 3]
+
+var b = [a, b, c]
+
+var c = [1, 2, c]
+
+var d = []
+
+
+function checkLetters(arr) {
+  if arr.length = 
+}
