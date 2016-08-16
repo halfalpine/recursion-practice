ed305e72643c0efe57d12cd5ef95e7db0e84d86e
halfalpine <rocksandwoks@gmail.com>
2016-08-16 09:10:52 -0400

    Add test variables

---
+++ rember.js
@@ -0,0 +1,11 @@
+let a1 = 'and', a2 = ['peanut', 'butter', 'and', 'jelly'];
+let b1 = 'cowboy', b2 = ['home', 'on', 'the', 'range'];
+let c1 = 'watermelon', c2 = ['watermelon'];
+let d1 = 'piza', d2 = [];
+
+function rember(str, arr){
+  if (arr.length = 0) {
+    return null;
+  }
+  
+}
