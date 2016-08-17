a46a3b215ab4f5684f18cdf0b372e6f84d31ae40
halfalpine <rocksandwoks@gmail.com>
2016-08-17 09:18:29 -0400

    Create test variables for firsts.js

---
+++ firsts.js
@@ -0,0 +1,11 @@
+// A function that takes one argument, an array, which is either an empty array or contains only non-empty arrays. It builds another array composed of the first value of each internal list.
+
+let a = [['apple', 'peach', 'pumpkin'], ['plum', 'pear', 'cherry'], ['grape', 'raisin', 'pea'], ['bean', 'carrot', 'eggplant']];
+let b = [['a', 'b'], ['c', 'd'], ['e', 'f']];
+let c = [];
+let d = [['five', 'plums'], ['four'], ['eleven', 'green', 'oranges']];
+let e = [[['five', 'plums'], four], ['eleven', 'green', 'oranges'], [['no'], 'more']];
+
+function firsts(arr){
+  
+}
