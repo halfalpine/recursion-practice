3fe28809f9647e11478d990f6709f58d69d138a3
halfalpine <rocksandwoks@gmail.com>
2016-08-17 09:06:17 -0400

    Add return statement for recursive 'else' statement

--- rember2.js
+++ rember2.js
@@ -1,16 +1,17 @@
 let a1 = 'and', a2 = ['peanut', 'butter', 'and', 'jelly'];
 let b1 = 'pizza', b2 = ['chocolate', 'milkshake'];
 let c1 = 'sundae', c2 = ['sundae', 'with', 'a', 'cherry', 'on', 'top'];
-let d1 = 'coffee', d2 = 'coffee';
 let e1 = 'tea', e2 = '';
 
 function rember(str, arr){
+  //console.log(str, arr);
   if (arr.length === 0) {
     return '';
   }
   if (str === arr[0]) {
-    return arr.substr(1);
+    return arr.slice(1);
   } else {
-    arr[0].concat(rember(str, arr.substr(1)));
+    console.log('recursing...');
+    return arr[0].concat(' ').concat(rember(str, arr.slice(1)));
   }
 }
