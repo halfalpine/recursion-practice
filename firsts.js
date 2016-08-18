18b9364ab2996c4bdb6de2e81eda9263921fced0
halfalpine <rocksandwoks@gmail.com>
2016-08-18 10:31:41 -0400

    Add .concat() to firsts.js

--- firsts.js
+++ firsts.js
@@ -1,5 +1,7 @@
 // A function that takes one argument, an array, which is either an empty array or contains only non-empty arrays. It builds another array composed of the first value of each internal list.
 
+// Right now this function works with only a single layer of nested arrays--more deeply nested arrays are not correctly concatenated.
+
 let a = [['apple', 'peach', 'pumpkin'], ['plum', 'pear', 'cherry'], ['grape', 'raisin', 'pea'], ['bean', 'carrot', 'eggplant']];
 let b = [['a', 'b'], ['c', 'd'], ['e', 'f']];
 let c = [];
@@ -10,6 +12,6 @@ function firsts(arr){
   if (arr.length === 0) {
     return [];
   } else {
-    return arr[0][0].push(firsts(arr.slice(1)));
+    return [].concat(arr[0][0]).concat(firsts(arr.slice(1)));
   }
 }
--- rember.js
+++ rember.js
@@ -4,8 +4,12 @@ let c1 = 'watermelon', c2 = ['watermelon'];
 let d1 = 'piza', d2 = [];
 
 function rember(str, arr){
-  if (arr.length = 0) {
+  if (arr.length === 0) {
     return null;
   }
-  
+  if (str === arr[0]) {
+    return arr.slice(1);
+  } else {
+    return arr[0].concat(return rember(str, arr.substr(1)));
+  }
 }
